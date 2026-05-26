4b99eb0f363a46b1ea641bb04a353772a8c3644d
kasim khan <kasimkkhan06@gmail.com>
2026-05-26 14:52:59 +0000

    corrected a bug related to commas

--- tools.py
+++ tools.py
@@ -17,7 +17,7 @@ def web_search(query : str) -> str:
 
     for r in results['results']:
         out.append(
-            f'Title: {r['title']}\nURL: {r['url']}\nSnippet: {r['content'][:300]}\n'
+            f"Title: {r['title']}\nURL: {r['url']}\nSnippet: {r['content'][:300]}\n"
         )
     return "\n-----\n".join(out)
 
